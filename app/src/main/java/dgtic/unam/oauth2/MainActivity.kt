2a32c9466904d5de421724ca9ba56e2d56895c5a
MoY8462 <moy.sav21@hotmail.com>
2022-12-23 22:13:02 -0600

    feat: :sparkles: authentication google

--- app/src/main/java/dgtic/unam/oauth2/MainActivity.kt
+++ app/src/main/java/dgtic/unam/oauth2/MainActivity.kt
@@ -1,20 +1,43 @@
 package dgtic.unam.oauth2
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import dgtic.unam.oauth2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
         validate()
+        sesiones()
+    }
+
+    private fun sesiones() {
+        val preferencias =
+            getSharedPreferences(getString(R.string.file_preferencia), Context.MODE_PRIVATE)
+        var email: String? = preferencias.getString("email", null)
+        var provedor: String? = preferencias.getString("provedor", null)
+        if (email != null && provedor != null) {
+            opciones(email, TipoProvedor.valueOf(provedor))
+        }
     }
 
     private fun validate() {
@@ -57,6 +80,22 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        //google acceso
+        iniciarActividad()
+        binding.google.setOnClickListener {
+            val conf =
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(
+                    getString(R.string.default_web_client_id)
+                )
+                    .requestEmail()
+                    .build()
+            val clienteGoogle = GoogleSignIn.getClient(this, conf)
+            clienteGoogle.signOut()
+            val signIn: Intent = clienteGoogle.signInIntent
+            activityResultLauncher.launch(signIn)
+        }
+
     }
 
     private fun alert() {
@@ -74,5 +113,37 @@ class MainActivity : AppCompatActivity() {
             putExtra("provedor", provesor.name)
         }
         startActivity(pasos)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.layoutAcceso.visibility = View.VISIBLE
+    }
+    private fun iniciarActividad() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    try {
+                        val account = task.getResult(ApiException::class.java)
+                        Toast.makeText(this, "Sign in successful", Toast.LENGTH_SHORT).show()
+                        if (account != null) {
+                            val credenciales =
+                                GoogleAuthProvider.getCredential(account.idToken, null)
+                            FirebaseAuth.getInstance().signInWithCredential(credenciales)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        opciones(account.email ?: "", TipoProvedor.GOOGLE)
+                                    } else {
+                                        alert()
+                                    }
+                                }
+                        }
+                    } catch (e: ApiException) {
+                        Toast.makeText(this, "Sign in failed: " + e.statusCode, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/dgtic/unam/oauth2/OpcionesActivity.kt
+++ app/src/main/java/dgtic/unam/oauth2/OpcionesActivity.kt
@@ -1,9 +1,14 @@
 package dgtic.unam.oauth2
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 import dgtic.unam.oauth2.databinding.ActivityOpcionesBinding
 
 enum class TipoProvedor {
@@ -13,6 +18,9 @@ enum class TipoProvedor {
 }
 class OpcionesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOpcionesBinding
+    private lateinit var googleSignInOption: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpcionesBinding.inflate(layoutInflater)
@@ -22,14 +30,37 @@ class OpcionesActivity : AppCompatActivity() {
         var email: String? = bundle?.getString("email")
         var provedor: String? = bundle?.getString("provedor")
         inicio(email ?: "", provedor ?: "")
+        //GUARDAR DATOS SESION
+        val preferencias =
+            getSharedPreferences(getString(R.string.file_preferencia), Context.MODE_PRIVATE).edit()
+        preferencias.putString("email", email)
+        preferencias.putString("provedor", provedor)
+        preferencias.apply()
     }
     private fun inicio(email: String, provedor: String) {
         binding.mail.text = email
         binding.provedor.text = provedor
         binding.closeSesion.setOnClickListener {
+            val preferencias = getSharedPreferences(
+                getString(R.string.file_preferencia),
+                Context.MODE_PRIVATE
+            ).edit()
+            preferencias.clear()
+            preferencias.apply()
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+        //google
+        if (provedor == TipoProvedor.GOOGLE.name) {
+            googleSignInOption =
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail().build()
+            googleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
+            val data = GoogleSignIn.getLastSignedInAccount(this)
+            if (data != null) {
+                Picasso.get().load(data.photoUrl).into(binding.img)
+            }
         }
     }
 }
